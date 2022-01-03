a09ba2a33c7eec2900b701a55f5ad73c3a1f2dbc
pakomercado0517 <sissyyayle0517@hotmail.com>
2022-01-02 21:43:21 -0600

    db on hosting

--- src/db.js
+++ src/db.js
@@ -2,12 +2,13 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DATABASE_URL } = process.env;
 
 // console.log('DB_HOST', DB_HOST);
 
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/servio`,
+  // `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/servio`,
+  DATABASE_URL,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
