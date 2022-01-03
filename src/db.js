892ce5e7cce27be4739981f6e7005b005ec746d2
pakomercado0517 <sissyyayle0517@hotmail.com>
2022-01-02 22:59:17 -0600

    pppp

--- src/db.js
+++ src/db.js
@@ -2,8 +2,7 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_TYPE, DB_NOMBRE } =
-  process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_TYPE, DB_NOMBRE } = process.env;
 
 // console.log('DB_HOST', DB_HOST);
 
