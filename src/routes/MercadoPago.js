28954589d76be3d6f935f24f157271cbfb0eae81
pakomercado0517 <sissyyayle0517@hotmail.com>
2022-01-10 09:55:25 -0600

    change localhost

--- src/routes/MercadoPago.js
+++ src/routes/MercadoPago.js
@@ -1,21 +1,25 @@
 const { Router } = require("express");
 const router = Router();
-const functions = require('../controllers/MercadoPago')
+const functions = require("../controllers/MercadoPago");
+const constants = {
+  localhost: "http://localhost:3001",
+  surge: "https://serv-io.surge.sh",
+};
 
 // POST
-router.post('/', functions.newTransaction)
+router.post("/", functions.newTransaction);
 
 // GET
-router.get('/succes', functions.succesTransaction)
+router.get("/succes", functions.succesTransaction);
 
-router.get('/pending', (req, res) =>{
-  console.log(req.query)
-  res.redirect("http://localhost:3000/")
-})
+router.get("/pending", (req, res) => {
+  console.log(req.query);
+  res.redirect(`${constants.surge}`);
+});
 
-router.get('/failure', (req, res) =>{
-  console.log(req.query)
-  res.redirect("http://localhost:3000/cart")
-})
+router.get("/failure", (req, res) => {
+  console.log(req.query);
+  res.redirect(`${constants.surge}/cart`);
+});
 
-module.exports= router;
+module.exports = router;
