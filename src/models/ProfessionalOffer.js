4a6960df11c30994e2db2f14285d9bce225b406b
pakomercado0517 <sissyyayle0517@hotmail.com>
2022-01-12 18:57:18 -0600

    v1234345

--- src/models/ProfessionalOffer.js
+++ src/models/ProfessionalOffer.js
@@ -1,35 +1,39 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-    return sequelize.define('ProfessionalOffer', {
+  return sequelize.define(
+    "ProfessionalOffer",
+    {
       name: {
         type: DataTypes.STRING,
         // allowNull: false,
       },
       description: {
-          type: DataTypes.STRING,
-          // allowNull: false,
+        type: DataTypes.STRING,
+        // allowNull: false,
       },
       status: {
         type: DataTypes.ENUM,
-        values: ["in offer", "pending to pay", "hired"]
+        values: ["in offer", "pending to pay", "hired", "rejected"],
         // allowNull: false,
       },
       price: {
-          type: DataTypes.INTEGER,
-          // allowNull: false,
+        type: DataTypes.INTEGER,
+        // allowNull: false,
       },
       duration: {
-          type: DataTypes.INTEGER,
-          // allowNull: false,
+        type: DataTypes.INTEGER,
+        // allowNull: false,
       },
       materials: {
-          type: DataTypes.BOOLEAN,
-          // allowNull: false,
+        type: DataTypes.BOOLEAN,
+        // allowNull: false,
       },
       guarantee_time: {
-          type: DataTypes.INTEGER,
-          // allowNull: false,
+        type: DataTypes.INTEGER,
+        // allowNull: false,
       },
-    }, {});
-}
+    },
+    {}
+  );
+};
