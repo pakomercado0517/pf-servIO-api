8c9ffb1c70855b3d492533d1e2ed320d9208b5ba
pakomercado0517 <sissyyayle0517@hotmail.com>
2022-01-12 19:04:44 -0600

    v234567

--- src/routes/ClientNeeds.js
+++ src/routes/ClientNeeds.js
@@ -1,15 +1,15 @@
 const { Router } = require("express");
 const router = Router();
-const needFunctions = require('../controllers/ClientNeedsControllers.js')
+const needFunctions = require("../controllers/ClientNeedsControllers.js");
 
-router.get('/all', needFunctions.getAllNeeds )//
-router.get('/need', needFunctions.getNeedByName )///
-router.get('/:id', needFunctions.getNeedsByUserId)//
-router.put('/:id', needFunctions.updateNeed)//
-router.post('/', needFunctions.newSpecificalNeed)//
-router.get('/need/:id', needFunctions.getById )//
-router.delete('/:id', needFunctions.deleteNeedById )
-router.post('/confirm/:token', needFunctions.confirm)//
-router.get('/confirm/:token', needFunctions.validarToken)//
+router.get("/all", needFunctions.getAllNeeds); //
+router.get("/need", needFunctions.getNeedByName); ///
+router.get("/:id", needFunctions.getNeedsByUserId); //
+router.put("/:id", needFunctions.updateNeed); //
+router.post("/", needFunctions.newSpecificalNeed); //
+router.get("/need/:id", needFunctions.getById); //
+router.delete("/:id", needFunctions.deleteNeedById);
+router.put("/confirm/:token", needFunctions.confirm); //
+router.get("/confirm/:token", needFunctions.validarToken); //
 
-module.exports= router;
+module.exports = router;
